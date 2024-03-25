26b1475f6042a309871e5969d61b034dac228371
lvcfxr <wateere@gmail.com>
2024-03-25 19:39:33 +0100

    v 1.0

--- main.cpp
+++ main.cpp
@@ -4,36 +4,17 @@
 
 using namespace std;
 
+int mySum(int a, int b) {
+
+    return a + b;
+}
+
 int main()
 {
-    const int size = 4;
-
-    int arr[size];
-    int user_input;
-
-    do {
-        cout << "Enter number(1 - 5): ";
-        cin >> user_input;
-    } while (user_input < 0 || user_input > 5);
+    int sum = mySum(3,4);
 
 
-    srand(time(0));
-
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 5;
-        if (arr[i] == user_input) {
-            cout << "\narr[" << i << "] = " << arr[i];
-        }
-    }
-
-    cout << "\n\nArray - ";
-
-
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-
-    cout << "\n";
+    cout << sum << endl;
 
     return 0;
 }
