50ec40390e86f5e37d87943a22ee15834baa2bba
lvcfxr <wateere@gmail.com>
2024-03-30 23:53:15 +0100

    v 1.0

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
 #include <algorithm>
-#include <ctime>
-#include <cctype>
+
 using namespace std;
 
+void sortTwoThirds(int arr[], int size) {
 
-int const SIZE = 5;
+    int sum = 0;
 
-
-void f(char arr[]) {
-    for (int i = 0; i < SIZE; i++) {
-        if (isupper(arr[i]))
-            arr[i] = tolower(arr[i]);
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
     }
+
+    if (sum / size > 0) {
+        int end = (size * 2) / 3;
+        sort(arr, arr + end);
+    }
+
+    else {
+        sort(arr, arr + size / 3);
+    }
+
+    reverse(arr + (size * 2) / 3, arr + size);
 }
 
+void printArray(int arr[], int size) {
 
-int main() {
-    char arr[SIZE]{ 'H', 'E', 'L', 'L', 'O' };
-    
-    f(arr);
-
-    for (int i = 0; i < SIZE; i++) {
-        cout << arr[i];
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
     }
 
     cout << endl;
+
+}
+
+int main() {
+
+    int arr[] = { 3, -1, 5, 2, -4, 8, 0, -6 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, size);
+
+    sortTwoThirds(arr, size);
+
+    printArray(arr, size);
 
     return 0;
 }
