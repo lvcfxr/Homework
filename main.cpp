954a9718b996cf5a763c61935e1a7d998fcae66e
lvcfxr <wateere@gmail.com>
2024-04-15 19:24:41 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -5,33 +5,35 @@
 #include <cstdlib>
 using namespace std;
 
-template <typename T>
-void sortArray(T arr[], int size) {
-    sort(arr, arr + size);
+
+int maxEl(int* arr, int size) {
+
+    int maxEl = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] > maxEl) {
+            maxEl = arr[i];
+        }
+    }
+    return maxEl;
 }
 
 int main() {
     const int size = 10;
     int arr[size];
 
-    
     srand(time(0));
 
     for (int i = 0; i < size; i++) {
         arr[i] = rand() % 100;
     }
 
-    cout << "Unsorted Array: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << ' ';
     }
 
-    sortArray(arr, size);
-
-    cout << "\nSorted Array: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << ' ';
-    }
+    int max = maxEl(arr, size);
+    cout << "\nBiggest element in array: " << max << endl;
 
     return 0;
 }
