0d1fa72bd20a982b35b187bbb44c6156bff44863
lvcfxr <wateere@gmail.com>
2024-04-19 19:19:42 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -5,69 +5,60 @@
 #include <cstdlib>
 using namespace std;
 
-bool isPrime(int number) {
+int maxEl(int *arr,int size) {
 
-    for (int i = 2; i * i <= number; ++i) {
-        if (number % i == 0) {
-            return false;
-        }
-    }
-
-    return number > 1;
-}
-
-int newarrSize;
-
-int* NonPrimaryNums(int* arr, int size) {
-
-    int count = 0;
+    int maxel = 0;
 
     for (int i = 0; i < size; i++) {
-        if (isPrime(arr[i])) {
-            count++;
-        }
-    }
-    
-    int* newarr = new int[count];
-
-    for (int i = 0, j = 0; i < size; i++) {
-        if (isPrime(arr[i])) {
-            newarr[j++] = arr[i];
+        if (arr[i] > maxel) {
+            maxel = arr[i];
         }
     }
 
-    newarrSize = count;
+    return maxel;
+}
 
-    delete[] arr;
+int minEl(int* arr, int size) {
 
-    return newarr;
+    int minel = arr[0];
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < minel) {
+            minel = arr[i];
+        }
+    }
+    return minel;
 }
 
 int main() {
-    
+
     int* ptr = nullptr, size;
 
     cout << "Enter size of array > ";
     cin >> size;
 
     ptr = new int[size];
-    
+
     srand(time(0));
 
     for (int i = 0; i < size; i++) {
         ptr[i] = rand() % 300;
         cout << ptr[i] << ' ';
     }
+
     cout << endl;
 
-    ptr = NonPrimaryNums(ptr, size);
-
     if (ptr != nullptr) {
-        for (int i = 0; i < newarrSize; i++) {
-            cout << ptr[i] << ' ';
-        }
+        auto maxel = maxEl(ptr, size);
+        auto minel = minEl(ptr, size);
+
+        cout << "Max element in array: " << maxel << "\nMin element in array: " << minel << endl;
         delete[] ptr;
     }
     
+
+
+    
+
     return 0;
 }
