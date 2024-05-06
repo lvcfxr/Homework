b2f273fd662e220e1dd706bae2d605d79b158a01
Рябов Валерiй Федорович <wateere@gmail.com>
2024-05-06 17:51:39 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -5,40 +5,128 @@
 #include <cstdlib>
 using namespace std;
 
-int* newReverseArray(int* arr, int size) {
-    int* newarr = new int[size];
-    int* start = arr;
-    int* end = arr + size - 1;
-    int* newStart = newarr;
+int pSize = 0;
+int nSize = 0;
+int zSize = 0;
 
-    while (start <= end) {
-        *newStart = *end;
-        newStart++;
-        end--;
+
+int* pArr(int* arr, int size) {
+    int count = 0;
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            count++;
+        }
+
+    }
+    pSize = count;
+
+    int* pArr = new int[pSize];
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            pArr[j] = arr[i];
+            j++;
+        }
+
     }
 
-    return newarr;
+    return pArr;
+}
+
+int* nArr(int* arr, int size) {
+    int count = 0;
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            count++;
+        }
+    }
+
+    nSize = count;
+
+    int* negArr = new int[nSize];
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            negArr[j] = arr[i];
+            j++;
+        }
+    }
+
+    return negArr;
+}
+
+int* zArr(int* arr, int size) {
+    int count = 0;
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] == 0) {
+            count++;
+        }
+    }
+
+    zSize = count;
+    int* zeroArr = new int[zSize];
+
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] == 0) {
+            zeroArr[j] = arr[i];
+            j++;
+        }
+    }
+
+
+    return zeroArr;
 }
 
 int main() {
-    const int size = 5;
-    int arr[size] = { 1, 2, 3, 4, 5 };
+    const int size = 15;
+    int arr[size];
+
+    srand(time(0));
 
     cout << "Original array: ";
     for (int i = 0; i < size; i++) {
+        arr[i] = (rand() % 201) - 100;
         cout << arr[i] << " ";
     }
-    cout << endl;
 
-    int* newarr = newReverseArray(arr, size);
+ 
 
-    cout << "New array in reverse order: ";
-    for (int i = 0; i < size; i++) {
-        cout << newarr[i] << " ";
+    int* posArr = pArr(arr, size);
+
+    cout << "\n\nPositive number arr: ";
+    for (int i = 0; i < pSize; i++) {
+        cout << posArr[i] << " ";
     }
+
+    if (posArr != nullptr) {
+        delete[] posArr;
+    }
+
+    int* negArr = nArr(arr, size);
+
+    cout << "\nNegative number arr: ";
+    for (int i = 0; i < nSize; i++) {
+        cout << negArr[i] << " ";
+    }
+
+    if (negArr != nullptr) {
+        delete[] negArr;
+    }
+
+    int* zeroArr = zArr(arr, size);
+    cout << "\nZeroes: ";
+    for (int i = 0; i < zSize; i++) {
+    cout << zeroArr[i] << " ";
+    }
+
     cout << endl;
 
-    delete[] newarr;
+    if (zeroArr != nullptr) {
+        delete[] zeroArr;
+    }
 
     return 0;
 }
