0ebb286331ea545054f7ede958d053b61f7e934b
Рябов Валерiй Федорович <wateere@gmail.com>
2024-05-13 18:06:48 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -5,51 +5,61 @@
 #include <cstdlib>
 using namespace std;
 
-struct students {
-    string name;
-    string surname;
-    int age;
-    int avg;
+void reverseArray(int* arr, int size) {
+    int* start = arr;
+    int* end = arr + size - 1;
 
-    void displayInfo() {
-        cout << "Name: " << name << endl;
-        cout << "Surname: " << surname << endl;
-        cout << "Age: " << age << endl;
-        cout << "Average grade: " << avg << endl;
+
+    while (start < end) {
+
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+
+        start++;
+        end--;
+    }
+}
+
+int main(){
+
+    int* arr = nullptr;
+    int size = 0;
+    int capacity = 0;
+    int num;
+
+    cout << "Enter numbers (enter 0 to exit):\n";
+    do {
+        cin >> num;
+        if (num != 0) {
+            if (size >= capacity) {
+                
+                capacity = (capacity == 0) ? 1 : capacity * 2;
+                int* temp = new int[capacity];
+                
+                for (int i = 0; i < size; ++i) {
+                    temp[i] = arr[i];
+                }
+                
+                delete[] arr;
+                arr = temp;
+            }
+            arr[size++] = num;
+        }
+    } while (num != 0);
+
+    reverseArray(arr, size);
+
+    cout << "Your array: ";
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
 
-    
-};
+    cout << endl;
 
-int main() {
-
-    students* Student = new students[3];
-
-    for (int i = 0; i < 3; ++i) {
-        cout << "Student " << i + 1 << endl;
-
-        cout << "\nEnter students name > ";
-        cin >> Student[i].name;
-
-        cout << "Enter students surname > ";
-        cin >> Student[i].surname;
-
-        cout << "Enter students age > ";
-        cin >> Student[i].age;
-
-        cout << "Enter students average grade > ";
-        cin >> Student[i].avg;
-
-        cout << "======================" << endl;
-    }
-
-    for (int i = 0; i < 3; i++) {
-        cout << "\nStudent " << i + 1 << endl;
-        Student[i].displayInfo();       
-    }
-    
-    if(Student != nullptr)
-        delete[] Student;
+    delete[] arr; 
 
     return 0;
 }
