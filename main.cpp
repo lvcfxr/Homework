5532b14d88593cc2519a99fcda7d2f21778f1fba
Рябов Валерiй Федорович <wateere@gmail.com>
2024-05-23 14:58:40 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -6,72 +6,17 @@
 using namespace std;
 
 
-class Array {
-public:
-    Array(int size = 5) {
-        if (size > 0) {
-            m_size = size;
-
-            m_ptr = new int[m_size];
-
-            srand(time(0));
-
-            for (int i = 0; i < m_size; i++) {
-                m_ptr[i] = rand() % 76;
-            }
-
-        }
-        else {
-            cout << "Size is too low!";
-            exit;
-        }
-
-
-    }
-
-    int findByIndex(int index) {
-        if ((index + 1) <= m_size)
-            return m_ptr[index + 1];
-        else {
-            cout << "Index is out of range!";
-            exit;
-        }
-    }
-
-    void print() {
-        if (m_size > 0) {
-            for (int i = 0; i < m_size; i++) {
-               cout << m_ptr[i] << ' ';
-            }
-        }
-        cout << endl;
-    }
-
-    ~Array() {
-        if (m_ptr != nullptr) {
-            delete[] m_ptr;
-        }
-    }
-
-private:
-    int m_size;
-    int* m_ptr;
-};
-
-
-
-
 int main() {
-    
-    Array arr1(0);
-    arr1.print();
+    const int MAX_SIZE = 100;
+    char array[MAX_SIZE];
+    cout << "Enter a string: ";
+    cin.getline(array, MAX_SIZE);
 
-    int user_input;
+    int used = strlen(array);
+    int free = MAX_SIZE - used - 1;
 
-    cout << "Enter index > ";
-    cin >> user_input;
-
-    arr1.findByIndex(user_input);
+    cout << "Number of used elements: " << used << endl;
+    cout << "Number of free elements: " << free << endl;
 
     return 0;
 }
