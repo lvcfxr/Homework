fdc29ada099810f36c6d396103d84b4f9202c593
Рябов Валерiй Федорович <wateere@gmail.com>
2024-06-16 23:28:42 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
-#include <algorithm>
 #include <cmath>
-#include <string>
+
+const double PI = 3.14159265359;
 
 using namespace std;
 
-class MyString {
-
-private:
-    string text;
-    
+class Shape {
 public:
-    MyString() : text("") {  };
+    double area() const { return 0; }
+    double perimeter() const { return 0; }
+};
 
-    MyString(const string &text) : text(text) {  };
+class Circle : public Shape {
+private:
+    double radius;
+public:
+    Circle(double r) : radius(r) {}
 
-    void operator()(int index = 0, int user_input = 0) {
-        int size = text.size();
-
-        if (user_input == 0) {
-            user_input = size;
-        }
-        
-        for (int i = index, j = 0; j < user_input; i++, j++) {
-            cout << text[i];
-        }
-        cout << endl;
-    }
-    
-    string add_elements(string el) {
-        text += el;
-        return text;
+    double area() const {
+        return PI * radius * radius;
     }
 
-    ~MyString() {
-
+    double perimeter() const {
+        return 2 * PI * radius;
     }
 };
 
+class Rectangle : public Shape {
+private:
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double area() const {
+        return width * height;
+    }
+
+    double perimeter() const {
+        return 2 * (width + height);
+    }
+};
+
+class Triangle : public Shape {
+private:
+    double a, b, c;
+public:
+    Triangle(double side1, double side2, double side3) : a(side1), b(side2), c(side3) {}
+
+    double area() const {
+        double s = (a + b + c) / 2;
+        return sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    double perimeter() const {
+        return a + b + c;
+    }
+};
 
 int main() {
+    Circle circle(5.0);
+    Rectangle rectangle(4.0, 6.0);
+    Triangle triangle(3.0, 4.0, 5.0);
 
-    MyString string("Hello, world!");
-    string(7,6);
-
-    string.add_elements("()");
-    string(0, 0);
-
+    cout << "Circle area: " << circle.area() << ", perimeter: " << circle.perimeter() << endl;
+    cout << "Rectangle area: " << rectangle.area() << ", perimeter: " << rectangle.perimeter() << endl;
+    cout << "Triangle area: " << triangle.area() << ", perimeter: " << triangle.perimeter() << endl;
 
     return 0;
 }
