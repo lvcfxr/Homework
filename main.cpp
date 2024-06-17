0aa31d105b91ea2d7d70c294a25e5b60d63c1488
Рябов Валерiй Федорович <wateere@gmail.com>
2024-06-17 19:21:16 +0200

    v 1.0

--- main.cpp
+++ main.cpp
@@ -1,93 +1,58 @@
 #include <iostream>
-#include <string>
+#include <list>
+#include <algorithm>
+#include <time.h>
 
 using namespace std;
 
-class Str {
-private:
-    string str;
-
-public:
-    Str() : str("") {}
-
-    Str(string str) : str(str) {}
-
-    Str(const Str& other) : str(other.str) {}
-
-    Str& operator=(const Str& other) {
-        if (this != &other) {
-            str = other.str;
-        }
-        return *this;
-    }
-
-    int length() const {
-        return str.size();
-    }
-
-    void clear() {
-        str.clear();
-    }
-
-    const string& strGet() const {
-        return str;
-    }
-
-    void print() const {
-        cout << str << endl;
-    }
-
-    friend Str operator+(const Str& other, const string& user_input);
-    friend Str& operator+=(Str& other, const string& user_input);
-    friend bool operator==(const Str& other, const string& user_input);
-    friend bool operator!=(const Str& other, const string& user_input);
-
-    ~Str() {}
-};
-
-Str operator+(const Str& other, const string& user_input) {
-    return Str(other.str + user_input);
-}
-
-Str& operator+=(Str& other, const string& user_input) {
-    other.str += user_input;
-    return other;
-}
-
-bool operator==(const Str& other, const string& user_input) {
-    return other.str == user_input;
-}
-
-bool operator!=(const Str& other, const string& user_input) {
-    return !(other == user_input);
-}
-
 int main() {
-    Str s1("Hello");
-    Str s2("World");
-    Str s3;
+    list<int> nums1(4, 0);
+    list<int> nums2(4, 0);
 
-    s3 = s1;
-    s3.print();
+    
+    srand(time(0));
 
-    Str s4 = s1 + " " + s2.strGet();
-    s4.print();
-
-    s1 += " C++";
-    s1.print();
-
-    if (s1 == "Hello C++") {
-        cout << "s1 is equal to 'Hello C++'" << endl;
+    for (auto it1 = nums1.begin(); it1 != nums1.end(); ++it1) {
+        *it1 = rand() % 100;
     }
 
-    if (s1 != s2.strGet()) {
-        cout << "s1 is not equal to s2" << endl;
+    
+    
+    for (auto it2 = nums2.begin(); it2 != nums2.end(); ++it2) {
+        *it2 = rand() % 100;
     }
 
-    cout << "Length of s1: " << s1.length() << endl;
+    
+    cout << "\nFirst list: ";
+    for (auto it1 = nums1.begin(); it1 != nums1.end(); ++it1) {
+        cout << *it1 << ' ';
+    }
+    nums1.sort();
+    
+    cout << "\nSecond list: ";
+    for (auto it2 = nums2.begin(); it2 != nums2.end(); ++it2) {
+        cout << *it2 << ' ';
+    }
+    nums2.sort();
+    
+    cout << "\nFirst list after sort: ";
+    for (auto it1 = nums1.begin(); it1 != nums1.end(); ++it1) {
+        cout << *it1 << ' ';
+    }
 
-    s1.clear();
-    s1.print();
+    cout << "\nSecond list after sort: ";
+    for (auto it2 = nums2.begin(); it2 != nums2.end(); ++it2) {
+        cout << *it2 << ' ';
+    }
+
+    nums1.merge(nums2);
+    nums1.sort();
+    
+    cout << "\nSorted list after merge: ";
+    for (auto it1 = nums1.begin(); it1 != nums1.end(); ++it1) {
+        cout << *it1 << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
